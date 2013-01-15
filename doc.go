d557d836f88b25452534944e94e614b124db8291
Keith Rarick <kr@xph.us>
2013-01-14 16:28:51 -0800

    convert example to a real Example

--- doc.go
+++ doc.go
@@ -1,7 +1,3 @@
-// Driver for the beanstalk protocol. See http://kr.github.com/beanstalkd/
-//
-// Example use:
-//   c, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
-//   c.Put([]byte("hello"), 1, 0, 120*time.Second)
-//   id, body, err := c.Reserve(5 * time.Second)
+// Package beanstalk provides a client for the beanstalk protocol.
+// See http://kr.github.com/beanstalkd/ for the server.
 package beanstalk
--- example_test.go
+++ example_test.go
@@ -6,6 +6,20 @@ import (
 	"time"
 )
 
+func Example() {
+	c, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
+	if err != nil {
+		panic(err)
+	}
+	c.Put([]byte("hello"), 1, 0, 120*time.Second)
+	id, body, err := c.Reserve(5 * time.Second)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("job", id)
+	fmt.Println(string(body))
+}
+
 func ExampleTubeSet_Reserve() {
 	c, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
 	if err != nil {
