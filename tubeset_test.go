f65c815ea43c2652349968ce2e3796df44034a28
John Kodumal <john@launchdarkly.com>
2014-12-09 11:22:06 -0800

    Fix tests, save (empty) godeps

--- tubeset_test.go
+++ tubeset_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestTubeSetReserve(t *testing.T) {
-	c := NewConn(mock("reserve-with-timeout 1\r\n", "RESERVED 1 1\r\nx\r\n"))
+	c := newConn(mock("reserve-with-timeout 1\r\n", "RESERVED 1 1\r\nx\r\n"), "", "")
 	id, body, err := c.Reserve(time.Second)
 	if err != nil {
 		t.Fatal(err)
@@ -23,7 +23,7 @@ func TestTubeSetReserve(t *testing.T) {
 }
 
 func TestTubeSetReserveTimeout(t *testing.T) {
-	c := NewConn(mock("reserve-with-timeout 1\r\n", "TIMED_OUT\r\n"))
+	c := newConn(mock("reserve-with-timeout 1\r\n", "TIMED_OUT\r\n"), "", "")
 	_, _, err := c.Reserve(time.Second)
 	if cerr, ok := err.(ConnError); !ok {
 		t.Log(err)
